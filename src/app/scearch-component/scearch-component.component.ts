df51aed2cc9cf8954345e9b01e349030ced38b6f
Jeeshan <Jeeshan.choudhary@ketto.org>
2022-09-02 13:42:10 +0530

    my-comm

--- src/app/scearch-component/scearch-component.component.ts
+++ src/app/scearch-component/scearch-component.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ApiServices } from 'src/api/api.services';
 
 @Component({
   selector: 'app-scearch-component',
@@ -8,15 +9,24 @@ import { Router } from '@angular/router';
 })
 export class ScearchComponentComponent implements OnInit {
   search?: string;
+  image?: string;
 
   Search(){
     this.router.navigate(['/search',this.search]);
   }
+
+  getImage(){
+    this.ApiServices.getPopular(1).subscribe((res:any) =>{
+      this.image = 'https://image.tmdb.org/t/p/original/'+ res.results[Math.floor(Math.random() * res.results.length)].backdrop_path
+    })
+  }
   constructor(
-    private router: Router
+    private router: Router,
+    private ApiServices: ApiServices
   ) { }
 
   ngOnInit(): void {
+    this.getImage()
   }
 
 }
